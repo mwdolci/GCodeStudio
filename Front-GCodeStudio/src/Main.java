9e8dbd419a769b92b326b4c3c6f0dde027b8ef16
Marco Dolci <marco.dolci@he-arc.ch>
2025-05-23 09:28:13 +0200

    Test version with FlatLaft

--- Front-GCodeStudio/src/Main.java
+++ Front-GCodeStudio/src/Main.java
@@ -1,5 +1,8 @@
 import java.nio.file.Paths;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import com.formdev.flatlaf.FlatLightLaf;
 
 // Pour lancer en bash:
 //javac *.java
@@ -8,6 +11,12 @@ import javax.swing.SwingUtilities;
 public class Main {
 
     public static void main(String[] args) {
+    	
+    	try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (Exception e) {
+            System.err.println("Erreur lors de l'application du thème FlatLaf");
+        }
 
         SwingUtilities.invokeLater(() -> {
             new MainWindow();  // Lance la fenêtre
